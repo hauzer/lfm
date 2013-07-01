ac8a8102ba57a63a148976b230ca9ef075da45a9
hauzer <hauzer@localhost>
2013-07-01 13:37:28 +0200

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
 
-setup(name='lfm',
-      version='1.0',
-      packages=['lfm', 'lfm.api'])
+setup(name="lfm",
+      version="1.0",
+      description="A Last.fm API interface.",
+      long_description="", #FIX
+      author="hauzer",
+      author_email="hauzer@gmx.com",
+      url="https://bitbucket.org/hauzer/lfm/",
+      download_url="https://bitbucket.org/hauzer/lfm/downloads",
+      license="", #FIX
+      packages=["lfm", "lfm.api"])
