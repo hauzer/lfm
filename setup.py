9e954dbf0f6cdee94fc5808c76154b8e16754820
hauzer <hauzer@localhost>
2013-07-25 02:39:30 +0200

    Changed to version 1.0.2.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 
 setup(name              = "lfmh",
-      version           = "1.0.1",
+      version           = "1.0.2",
       packages          = find_packages(),
       install_requires  = ["requests"],
 
