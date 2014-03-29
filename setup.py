a9ba6ff812ea56a836f373546d9eefa1145a03dd
?????? ???????????? <hauzer@gmx.com>
2014-03-29 19:47:05 +0100

    Bumped to 1.0.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name              = "{}h".format(info.NAME),
       download_url      = "https://bitbucket.org/{}/{}/downloads".format(info.AUTHOR_NICK, info.NAME),
       
       classifiers = [
-                     "Development Status :: 3 - Alpha",
+                     "Development Status :: 4 - Beta",
                      "Intended Audience :: Developers",
                      "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
                      "Operating System :: OS Independent",
